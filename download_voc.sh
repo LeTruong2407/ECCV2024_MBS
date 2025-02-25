c4e5312389b927b55aa6d7c9bb87a38b5f32661a
Le Van Truong <89858331+LeTruong2407@users.noreply.github.com>
2025-02-25 13:25:31 +0700

    Update download_voc.sh

--- download_voc.sh
+++ download_voc.sh
@@ -2,7 +2,7 @@ cd data_root
 
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
 tar -xf VOCtrainval_11-May-2012.tar
-wget http://cs.jhu.edu/~cxliu/data/SegmentationClassAug.zip
+gdown 1ECQRCYKWdZSsdzvAtYUXXExxQWcRySvo
 wget http://cs.jhu.edu/~cxliu/data/SegmentationClassAug_Visualization.zip
 wget http://cs.jhu.edu/~cxliu/data/list.zip
 rm VOCtrainval_11-May-2012.tar
